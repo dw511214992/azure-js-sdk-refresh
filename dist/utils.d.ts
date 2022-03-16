83be67fb126b0f3af0a87a1a6c51eada8e25383b
dw511214992 <dw511214992@gmail.com>
2022-03-16 13:29:52 +0800

    commit

--- dist/utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function checkOutSwaggerRepo(swaggerRepo: string, commitId: any): void;
-export declare function commit(swaggerRepo: string, packageName: string): void;
-export declare function generateCodes(options: any, readme: string): void;
